6ccbab6e3e23d0e858ebb130f7f0da51bd49d908
JaimeBonBol <jaimebb2002@gmail.com>
2025-03-10 18:26:24 +0100

    Comenzando aprendizaje JavaScript

---
+++ JavaScript/Basic/01-variables.js
@@ -0,0 +1,29 @@
+//var Esta en desuso y se desaconseja
+
+//hoisting.
+console.log(helloWorld)
+
+var helloWorld = "¡Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+//Estas son las formas principales de declarar variables:
+
+//let
+let helloWorld2 = "¡Hola, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+//const
+
+const helloWorld3 = "¡Hola, JavaScript 3!"
+console.log(helloWorld3)
+
+//No puedes reasignar una variable que es constante.
+/*Error
+helloWorld3 = "¡Hola de nuevo, JavaScript 3!"
+console.log(helloWorld3)*/
