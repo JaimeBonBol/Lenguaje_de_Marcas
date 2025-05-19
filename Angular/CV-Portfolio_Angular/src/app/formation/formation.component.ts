d77728803e270f1b28d37bf259caec00e5c75b27
JaimeBonBol <jaimebb2002@gmail.com>
2025-05-19 20:27:31 +0200

    Finalización Portfolio con Angular, utilizando angular material

--- Angular/CV-Portfolio_Angular/src/app/formation/formation.component.ts
+++ Angular/CV-Portfolio_Angular/src/app/formation/formation.component.ts
@@ -23,9 +23,9 @@ export class FormationComponent {
 
   displayedColumns: string[] = ['title', 'where', 'during'];
 
-  course1 = new Course('Ciclo Formativo Grado Superior Automatización y Robótica', 'Salesianos C.D.P San Luis Rey, Palma del Río.', '2020 - 2022');
+  course1 = new Course('Ciclo Formativo Grado Superior Desarrollo de Aplicaciones Multiplataforma', 'IES Antonio Gala, Palma del Río.', '2024 - ACTUALIDAD');
   course2 = new Course('Curso de Especialización en Fabricación Inteligente', 'IES Zoco, Córdoba.', '2022 - 2023');
-  course3 = new Course('Ciclo Formativo Grado Superior Desarrollo de Aplicaciones Multiplataforma', 'IES Antonio Gala, Palma del Río.', '2024 - ACTUALIDAD');
+  course3 = new Course('Ciclo Formativo Grado Superior Automatización y Robótica', 'Salesianos C.D.P San Luis Rey, Palma del Río.', '2020 - 2022');
   
   courses: Course[] = [this.course1, this.course2, this.course3];
 }
